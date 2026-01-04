93ab7bd3e6996308fea56343cd507f9da2b8b6be
crystalnat <nawahikalu@gmail.com>
2026-01-04 21:56:53 +0700

    ok

--- app/Filament/Resources/SuratResource.php
+++ app/Filament/Resources/SuratResource.php
@@ -11,6 +11,7 @@ use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 
 class SuratResource extends Resource
